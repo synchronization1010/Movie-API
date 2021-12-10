8acb049cb72639b0aa0f0100c6c7256533a79a34
synchronization1010 <85970124+synchronization1010@users.noreply.github.com>
2021-12-10 16:19:01 -0600

    added morgan and express functions

--- index.js
+++ index.js
@@ -1,59 +1,80 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
-
-app.get('/movies', (req, res) => {
-    res.json(movies);
-});
+app.use(morgan('common'));
 
 let movies = [
     {
       title: 'Home Alone',
       genre: 'Comedy',
-      releaseYear: 1990
+    year: 1990
     },
     {
       title: 'The Fifth Element',
       genre: 'Sci-Fi',
-      releasedYear: 1997
+      year: 1997
     },
     {
       title: 'The Mask of Zoro',
       genre:'Action',
-      releasedYear: 1998
+      year: 1998
     },
     {
       title: 'The Truman Show',
       genre:'Drama',
-      releasedYear: 1998
+      year: 1998
     },
     {
       title: 'Matrix',
       genre: 'Action',
-      releasedYear: 1990
+      year: 1990
     },
     {
       title: 'Edward Scissor Hands',
       genre: 'Drama',
-      releasedYear: 1990
+      year: 1990
     },
     {
       title: 'Liar Liar',
       genre: 'Comedy',
-      releasedYear: 1997
+      year: 1997
     },
     {
       title: 'The Green Mile',
       genre: 'Drama',
-      releasedYear: 1999
+      year: 1999
     },
     {
       title: 'Armageddon',
       genre: 'Action',
-      releasedYear: 1998
+      year: 1998
     },
     {
       title: 'Big Daddy',
       genre: 'Comedy',
-      releasedYear: 1999
-    }
-  ];
+      year: 1999
+    }   
+];
+
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+app.get('/', (req, res) =>{
+    res.send('You like movies from the 90\'s, we like movies from the 90\'s!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile ('public/documentation.html', {root: __dirname }); //respond through express.static
+});
+
+app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
