24af0aa9b582cc2964066d6a5851708608aba61d
synchronization1010 <85970124+synchronization1010@users.noreply.github.com>
2022-01-14 17:06:03 -0600

    URI correction

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //All Movies
 app.get('/movies', (req, res) => {
